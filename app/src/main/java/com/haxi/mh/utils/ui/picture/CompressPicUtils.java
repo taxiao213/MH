3ccbfbae51876b984cfe84a5a763749cbebd27c8
yin13753884368@163.com <han13593427958>
2019-01-17 14:14:15 +0800

    压缩工具类修改

--- app/src/main/java/com/haxi/mh/utils/ui/picture/CompressPicUtils.java
+++ app/src/main/java/com/haxi/mh/utils/ui/picture/CompressPicUtils.java
@@ -1,99 +1,101 @@
 package com.haxi.mh.utils.ui.picture;
 
-import android.annotation.SuppressLint;
-import android.content.ContentUris;
-import android.content.Context;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.text.TextUtils;
+
+
+import com.haxi.mh.constant.HConstants;
 
 import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
 
 /**
- * 图片压缩 压缩在指定路径下
- * Created by Han on 2017/12/25
- * Email:yin13753884368@163.com
- * CSDN:http://blog.csdn.net/yin13753884368/article
- * Github:https://github.com/yin13753884368
+ * 图片压缩工具类
+ * Created by han on 2018/11/06
  */
 public class CompressPicUtils {
-    static String path = Environment.getExternalStorageDirectory().toString() + "/picture/";
 
-    public static String getCompToRealPath(Context context, String strpath) {
-        String realPath = null;
-        Bitmap bt = CompressPicUtils.getimage(strpath);
-        try {
-            Uri ur = Uri.fromFile(CompressPicUtils.saveFile(bt, path, UUID.randomUUID().toString() + ".jpg"));
-            realPath = CompressPicUtils.getRealPathFromUri(context, ur);
-            return realPath;
-        } catch (IOException e) {
-            e.printStackTrace();
+    static String path = null;//根据不同的type 保存到不同目录下  1 个人头像图片 2 聊天照片压缩
+
+    /**
+     * @param strpath 图片路径
+     * @param type    1 需要压缩图片的文件路径   2聊天照片全路径
+     */
+    public static String getCompToRealPath(String strpath, int type) {
+        if (TextUtils.isEmpty(strpath)) {
+        } else if (strpath.endsWith("gif")) {
+            path = strpath;
+        } else {
+            try {
+                if (type == 1) {
+                    path = HConstants.FILE_COMPRESS_NAME;
+                }
+                if (strpath.contains("/")) {
+                    int i = strpath.lastIndexOf("/");
+                    String imagePath = strpath.substring(i + 1);
+                    Bitmap bt = getImage(strpath, type);
+                    int degree = readPictureDegree(strpath);
+                    Bitmap rotaingBt = rotaingImageView(degree, bt);
+                    path = saveFile(rotaingBt, path, imagePath);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        return realPath;
+        return path;
     }
 
     /**
-     * 图片按比例大小压缩方法（根据Bitmap图片压缩）
+     * 图片按比例大小压缩方法
      *
-     * @param image
+     * @param srcPath （根据路径获取图片并压缩）
+     * @param type
      * @return
      */
-    public static Bitmap comp(Bitmap image) {
+    public static Bitmap getImage(String srcPath, int type) {
+        Bitmap src = BitmapFactory.decodeFile(srcPath);
+        if (src == null) return null;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        image.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        if (baos.toByteArray().length / 1024 > 1024) {//判断如果图片大于1M,进行压缩避免在生成图片（BitmapFactory.decodeStream）时溢出
-            baos.reset();//重置baos即清空baos
-            image.compress(Bitmap.CompressFormat.PNG, 50, baos);//这里压缩50%，把压缩后的数据存放到baos中
+        src.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] bytes = baos.toByteArray();
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+        int w = options.outWidth;
+        int h = options.outHeight;
+        float hh = 0f;
+        float ww = 0f;
+        // 现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
+        if (type == 1) {
+            hh = 480f;// 这里设置高度为800f---960*540
+            ww = 480f;// 这里设置宽度为480f
+        } else if (type == 2) {
+            hh = 1920f;
+            ww = 1080f;
         }
-        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
-        BitmapFactory.Options newOpts = new BitmapFactory.Options();
-        //开始读入图片，此时把options.inJustDecodeBounds 设回true了
-        newOpts.inJustDecodeBounds = true;
-        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
-        newOpts.inJustDecodeBounds = false;
-        int w = newOpts.outWidth;
-        int h = newOpts.outHeight;
-        //现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
-        float hh = 800f;//这里设置高度为800f
-        float ww = 480f;//这里设置宽度为480f
-        //缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
-        int be = 1;//be=1表示不缩放
-        if (w > h && w > ww) {//如果宽度大的话根据宽度固定大小缩放
-            be = (int) (newOpts.outWidth / ww);
-        } else if (w < h && h > hh) {//如果高度高的话根据宽度固定大小缩放
-            be = (int) (newOpts.outHeight / hh);
+        // 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;// be=1表示不缩放
+        if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
+            be = (int) (options.outWidth / ww);
+        } else if (w < h && h > hh) {// 如果高度高的话根据宽度固定大小缩放
+            be = (int) (options.outHeight / hh);
         }
         if (be <= 0)
             be = 1;
-        newOpts.inSampleSize = be;//设置缩放比例
-        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;//降低图片从ARGB888到RGB565
-        //重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
-        isBm = new ByteArrayInputStream(baos.toByteArray());
-        bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
-        return bitmap;//压缩好比例大小后再进行质量压缩
+        options.inSampleSize = be;// 设置缩放比例
+        options.inPreferredConfig = Bitmap.Config.RGB_565;//降低图片从ARGB888到RGB565
+        options.inJustDecodeBounds = false;
+        // 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+        if (!src.isRecycled()) src.recycle();
+        return compressImage(bitmap);// 压缩好比例大小后再进行质量压缩
     }
 
-    public static Bitmap readBitMap(Context context, int resId) {
-        BitmapFactory.Options opt = new BitmapFactory.Options();
-        opt.inPreferredConfig = Bitmap.Config.RGB_565;
-        opt.inPurgeable = true;
-        opt.inInputShareable = true;
-        // 获取资源图片
-        InputStream is = context.getResources().openRawResource(resId);
-        return BitmapFactory.decodeStream(is, null, opt);
-    }
 
     /**
      * 质量压缩方法
@@ -103,192 +105,116 @@ public class CompressPicUtils {
      */
     private static Bitmap compressImage(Bitmap image) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//质量压缩方法，这里100表示不压缩(代表的是压缩率)，把压缩后的数据存放到baos中
+        if (image != null) {
+            image.compress(Bitmap.CompressFormat.JPEG, 100, baos);//质量压缩方法，这里100表示不压缩(代表的是压缩率)，把压缩后的数据存放到baos中
+        }
+        byte[] bytes = baos.toByteArray();
         int options = 100;
-        while (baos.toByteArray().length / 1024 > 100) {//循环判断如果压缩后图片是否大于200kb,大于继续压缩
+        while (bytes.length / 1024 > 200) {//循环判断如果压缩后图片是否大于200kb,大于继续压缩
             baos.reset();//重置baos即清空baos
             options -= 10;//每次都减少10
-            image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options%，把压缩后的数据存放到baos中
+            if (image != null) {
+                image.compress(Bitmap.CompressFormat.JPEG, options, baos);//这里压缩options%，把压缩后的数据存放到baos中
+            }
+            bytes = baos.toByteArray();
         }
-        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());//把压缩后的数据baos存放到ByteArrayInputStream中
-        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
-        return bitmap;
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
 
     }
 
-
-    /**
-     * 图片按比例大小压缩方法
-     *
-     * @param srcPath （根据路径获取图片并压缩）
-     * @return
-     */
-    public static Bitmap getimage(String srcPath) {
-
-        BitmapFactory.Options newOpts = new BitmapFactory.Options();
-        // 开始读入图片，此时把options.inJustDecodeBounds 设回true了
-        newOpts.inJustDecodeBounds = true;
-        Bitmap bitmap = BitmapFactory.decodeFile(srcPath, newOpts);// 此时返回bm为空
-
-        newOpts.inJustDecodeBounds = false;
-        int w = newOpts.outWidth;
-        int h = newOpts.outHeight;
-        // 现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
-        float hh = 800f;// 这里设置高度为800f---960*540
-        float ww = 480f;// 这里设置宽度为480f
-        // 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
-        int be = 1;// be=1表示不缩放
-        if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
-            be = (int) (newOpts.outWidth / ww);
-        } else if (w < h && h > hh) {// 如果高度高的话根据宽度固定大小缩放
-            be = (int) (newOpts.outHeight / hh);
-        }
-        if (be <= 0)
-            be = 1;
-        newOpts.inSampleSize = be;// 设置缩放比例
-        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;//降低图片从ARGB888到RGB565
-        // 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
-        bitmap = BitmapFactory.decodeFile(srcPath, newOpts);
-        return compressImage(bitmap);// 压缩好比例大小后再进行质量压缩
-    }
-
-    /**
-     * Get bitmap from specified image path
-     *
-     * @param imgPath
-     * @return
-     */
-    public static Bitmap getBitmap(String imgPath) {
-        // Get bitmap through image path
-        BitmapFactory.Options newOpts = new BitmapFactory.Options();
-        newOpts.inJustDecodeBounds = false;
-        newOpts.inPurgeable = true;
-        newOpts.inInputShareable = true;
-        // Do not compress
-        newOpts.inSampleSize = 1;
-        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;
-        return BitmapFactory.decodeFile(imgPath, newOpts);
-    }
 
     /**
      * 将Bitmap转换成文件
      * 保存文件
      *
-     * @param bm
-     * @param fileName
-     * @throws IOException
+     * @param src      源文件
+     * @param path     文件路径
+     * @param fileName 文件名称
      */
-    public static File saveFile(Bitmap bm, String path, String fileName) throws IOException {
-        File dirFile = new File(path);
-        if (!dirFile.exists()) {
-            dirFile.mkdir();
-        }
-        File myCaptureFile = new File(path, fileName);
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(myCaptureFile));
-        bm.compress(Bitmap.CompressFormat.JPEG, 80, bos);
-        bos.flush();
-        bos.close();
-        return myCaptureFile;
-    }
-
-    /**
-     * 根据Uri获取图片的绝对路径
-     *
-     * @param context 上下文对象
-     * @param uri     图片的Uri
-     * @return 如果Uri对应的图片存在, 那么返回该图片的绝对路径, 否则返回null
-     */
-    public static String getRealPathFromUri(Context context, Uri uri) {
-        int sdkVersion = Build.VERSION.SDK_INT;
-        if (sdkVersion >= 19) { // api >= 19
-            return getRealPathFromUriAboveApi19(context, uri);
-        } else { // api < 19
-            return getRealPathFromUriBelowAPI19(context, uri);
-        }
-    }
-
-    /**
-     * 适配api19以下(不包括api19),根据uri获取图片的绝对路径
-     *
-     * @param context 上下文对象
-     * @param uri     图片的Uri
-     * @return 如果Uri对应的图片存在, 那么返回该图片的绝对路径, 否则返回null
-     */
-    private static String getRealPathFromUriBelowAPI19(Context context, Uri uri) {
-        return getDataColumn(context, uri, null, null);
-    }
-
-    /**
-     * 适配api19及以上,根据uri获取图片的绝对路径
-     *
-     * @param context 上下文对象
-     * @param uri     图片的Uri
-     * @return 如果Uri对应的图片存在, 那么返回该图片的绝对路径, 否则返回null
-     */
-    @SuppressLint("NewApi")
-    private static String getRealPathFromUriAboveApi19(Context context, Uri uri) {
-        String filePath = null;
-        if (DocumentsContract.isDocumentUri(context, uri)) {
-            // 如果是document类型的 uri, 则通过document id来进行处理
-            String documentId = DocumentsContract.getDocumentId(uri);
-            if (isMediaDocument(uri)) { // MediaProvider
-                // 使用':'分割
-                String id = documentId.split(":")[1];
-
-                String selection = MediaStore.Images.Media._ID + "=?";
-                String[] selectionArgs = {id};
-                filePath = getDataColumn(context, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection, selectionArgs);
-            } else if (isDownloadsDocument(uri)) { // DownloadsProvider
-                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(documentId));
-                filePath = getDataColumn(context, contentUri, null, null);
-            }
-        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
-            // 如果是 content 类型的 Uri
-            filePath = getDataColumn(context, uri, null, null);
-        } else if ("file".equals(uri.getScheme())) {
-            // 如果是 file 类型的 Uri,直接获取图片对应的路径
-            filePath = uri.getPath();
-        }
-        return filePath;
-    }
-
-    /**
-     * 获取数据库表中的 _data 列，即返回Uri对应的文件路径
-     *
-     * @return
-     */
-    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
-        String path = null;
-
-        String[] projection = new String[]{MediaStore.Images.Media.DATA};
-        Cursor cursor = null;
+    public static String saveFile(Bitmap src, String path, String fileName) {
+        if (src == null) return "";
+        BufferedOutputStream bos = null;
+        File myCaptureFile = null;
         try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
-            if (cursor != null && cursor.moveToFirst()) {
-                int columnIndex = cursor.getColumnIndexOrThrow(projection[0]);
-                path = cursor.getString(columnIndex);
+            File dirFile = new File(path);
+            if (!dirFile.exists()) {
+                dirFile.mkdirs();
+            }
+            myCaptureFile = new File(path, fileName);
+            if (!myCaptureFile.exists()) {
+                myCaptureFile.createNewFile();
+            }
+            bos = new BufferedOutputStream(new FileOutputStream(myCaptureFile));
+            src.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+            if (!src.isRecycled()) src.recycle();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bos != null) {
+                    bos.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return myCaptureFile == null ? "" : myCaptureFile.getAbsolutePath();
+    }
+
+
+    /**
+     * 读取照片旋转角度
+     *
+     * @param path 照片路径
+     * @return 角度
+     */
+    public static int readPictureDegree(String path) {
+        int degree = 0;
+        try {
+            ExifInterface exifInterface = new ExifInterface(path);
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
             }
         } catch (Exception e) {
-            if (cursor != null) {
-                cursor.close();
-            }
+            e.printStackTrace();
         }
-        return path;
+        return degree;
     }
 
     /**
-     * @param uri the Uri to check
-     * @return Whether the Uri authority is MediaProvider
+     * 旋转图片
+     *
+     * @param angle  被旋转角度
+     * @param bitmap 图片对象
+     * @return 旋转后的图片
      */
-    private static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri the Uri to check
-     * @return Whether the Uri authority is DownloadsProvider
-     */
-    private static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    public static Bitmap rotaingImageView(int angle, Bitmap bitmap) {
+        if (bitmap == null) return null;
+        Bitmap returnBm = null;
+        // 根据旋转角度，生成旋转矩阵
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        try {
+            // 将原始图片按照旋转矩阵进行旋转，并得到新的图片
+            returnBm = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        } catch (Exception e) {
+        }
+        if (returnBm == null) {
+            returnBm = bitmap;
+        }
+        if (bitmap != returnBm) {
+            bitmap.recycle();
+        }
+        return returnBm;
     }
 }
